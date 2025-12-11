2e525bf434764ca62186fc18c801b577fcde2ac1
绣虎 <121874694@qq.com>
2025-12-11 15:27:33 +0800

    feat

---
+++ app-webview.tsx
@@ -0,0 +1,249 @@
+import { useRef } from "react";
+import WebView from "react-native-webview";
+
+import { PermissionCode, requestMultiplePermissions, requestPermission } from 'xm-rn-permissions'
+import { openSettings, RESULTS } from "react-native-permissions";
+import { Linking } from "react-native";
+import {
+    addCalendarEvents, buildWebviewEnv, createRiskBuilder, createSupermarketReview, getLocation,
+    openCamera, openGallery, selectContactPhone
+} from "xm-rn-risk";
+
+// isH5Login
+const NOT_NEED_REPLAY = "__NOT_NEED_REPLAY__"
+const REPLAY_WAIT_APP_CALLBACK = "__REPLAY_WAIT_APP_CALLBACK__"
+export const H5Events = {
+    getAllPermission: "getAllPermission",
+    getPermission: "getPermission",
+    openAppSettings: "openAppSettings",
+    imageFromGallery: "imageFromGallery",
+    imageFromCamera: "imageFromCamera",
+    launchUrl: "launchUrl",
+    pickContact: "pickContact",
+    requestInAppReview: "requestInAppReview",
+    uploadAllDeviceData: "uploadAllDeviceData",
+    uploadDeviceData: "uploadDeviceData",
+    logOut: "logOut",
+    getEnv: "getEnv",
+    getUser: "getUser",
+    addCalendar: "addCalendar",
+    openOCR: "openOCR",
+    webLoginSuccess: "webLoginSuccess",
+    getLocation: "getLocation",
+    ocrResponse: "ocrResponse",
+    reload: "reload",
+}
+
+interface I_AppWebViewEnv {
+    url: string
+    i18n: string
+    country: string
+    afId: string
+    statusBarHeight: number
+    ownerShip: string
+    openOCR: boolean
+    hybridVersion: number
+    firstKey: string
+
+    permissions?: PermissionCode[]
+
+    builder: ReturnType<typeof createRiskBuilder>
+
+    onLogout: () => Promise<void>
+    onWebLoginSuccess: (loginInfo?: any) => void
+    onOpenOCR: (data: any) => void
+    onGetUserInfo: () => { token: string; cellular: string; uuid: string; userIsTester: string }
+    onUpload: (data: any) => Promise<void>
+    onGetSupermarketUrl: () => Promise<string>
+}
+export function AppWebView(
+    props: I_AppWebViewEnv
+) {
+
+    const {
+        permissions = [],
+        onLogout, onWebLoginSuccess, onOpenOCR, onGetUserInfo, onUpload, builder, onGetSupermarketUrl,
+        url,
+        i18n, country, afId,
+        statusBarHeight, ownerShip, openOCR,
+        hybridVersion, firstKey,
+
+    } = props;
+    const webviewRef = useRef<WebView>(null);
+    function replayMessage(eventType: string, data: any) {
+        if (!webviewRef.current) {
+            return console.error(`[xm-rn-webview]: 回复事件失败!,webview ref不存在!`);
+        };
+
+        // 不需要回复
+        if (data === NOT_NEED_REPLAY) {
+            return console.log(`[xm-rn-webview]: ${eventType}事件结束-->无需回复)`);
+        }
+
+        // 需要等待App主动回复
+        if (data === REPLAY_WAIT_APP_CALLBACK) {
+            return console.log(`[xm-rn-webview]: ${eventType}已事件执行-->等待app主动回复)`);
+        }
+        console.log(`[xm-rn-webview]: 回复${eventType}-->${JSON.stringify(data)})`);
+        webviewRef.current.injectJavaScript(`window?.postMsg("${eventType}",  JSON.stringify(${JSON.stringify(data)}));`,)
+    }
+
+    /**
+     * 格式化H5事件
+     * @param value 
+     * @returns 
+     */
+    function parseH5Event<T = any>(value: any) {
+        try {
+            return JSON.parse(value) as { type: keyof typeof H5Events, data: T };
+        } catch (error: any) {
+            throw new Error(`[xm-rn-webview]: 解析事件失败!,事件字符串: ${value}`)
+        }
+    }
+    /**
+     * 从H5接受到消息
+     */
+    async function receiveMessageFromH5(event: any) {
+        try {
+
+            const eventInfo = parseH5Event(event.nativeEvent.data);
+            const eventType = eventInfo.type;
+            const eventData = eventInfo.data;
+            console.log(`[xm-rn-webview]: 接收事件${eventType}-->${eventData})`);
+            const isPermissionDeniedForever = (value: any) => value == RESULTS.BLOCKED || value === RESULTS.LIMITED || value === RESULTS.UNAVAILABLE
+            if (eventType === H5Events.getAllPermission) {
+                const result = await requestMultiplePermissions(permissions ?? []);
+                const responseData = {
+                    isGranted: result.every(item => item.status === RESULTS.GRANTED || item.status == "ignored_permission"),
+                    isPermanentlyDenied: result.some(item => isPermissionDeniedForever(item.status)),
+                    rejectPermission: result.filter(item => (item.status != RESULTS.GRANTED && item.status != "ignored_permission")).map(item => item.serviceCode),
+                    alwaysRejectPermission: result.filter(item => isPermissionDeniedForever(item.status)).map(item => item.serviceCode)
+                }
+                return replayMessage(eventType, responseData)
+
+            }
+
+            if (eventType === H5Events.getPermission) {
+                const result = await requestPermission?.(eventData?.type);
+                const responseData = {
+                    isGranted: (result == RESULTS.GRANTED || result == "ignored_permission"),
+                    isPermanentlyDenied: isPermissionDeniedForever(result)
+                }
+                return replayMessage(eventType, responseData)
+            }
+
+            if (eventType === H5Events.openAppSettings) {
+                await openSettings()
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+
+            if (eventType === H5Events.imageFromCamera) {
+                const requestResult = await requestPermission(PermissionCode.Camera);
+                if (requestResult != RESULTS.GRANTED) return
+                const result = await openCamera();
+                return replayMessage(eventType, result)
+            }
+
+            if (eventType === H5Events.imageFromGallery) {
+                const result = await openGallery();
+                return replayMessage(eventType, result)
+            }
+
+            if (eventType === H5Events.launchUrl) {
+                Linking.openURL(eventData?.url)
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+
+            if (eventType === H5Events.pickContact) {
+                const result = await selectContactPhone();
+                return replayMessage(eventType, result)
+            }
+
+            if (eventType === H5Events.requestInAppReview) {
+                const open = await createSupermarketReview(onGetSupermarketUrl)
+                await open()
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+
+            if (eventType === H5Events.uploadAllDeviceData) {
+                await requestMultiplePermissions(permissions);
+                const dataList = await builder(permissions)
+                const promiseList = dataList.map(item => onUpload?.(item))
+                await Promise.allSettled(promiseList);
+                return replayMessage(eventType, "")
+            }
+
+            if (eventType === H5Events.uploadDeviceData) {
+                await requestMultiplePermissions(eventData.type);
+                const dataList = await builder(eventData.type)
+                const promiseList = dataList.map(item => onUpload?.(item))
+                await Promise.allSettled(promiseList);
+                return replayMessage(eventType, "")
+            }
+            if (eventType === H5Events.logOut) {
+                await onLogout();
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+            if (eventType === H5Events.getEnv) {
+                const temp = await buildWebviewEnv()
+                return replayMessage(H5Events.getEnv, {
+                    ...temp,
+                    i18n, country, afId,
+                    statusBarHeight, ownerShip, openOCR,
+                    hybridVersion, firstKey,
+                })
+            }
+            if (eventType === H5Events.getUser) {
+                const userInfo = onGetUserInfo?.();
+                return replayMessage(eventType, userInfo)
+            }
+            if (eventType === H5Events.addCalendar) {
+                const requestResult = await requestPermission(PermissionCode.Calendar);
+                if (requestResult != RESULTS.GRANTED) return
+                await addCalendarEvents(eventData)
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+
+            // 该事件为特殊事件 不需要返回
+            if (eventType === H5Events.openOCR) {
+                onOpenOCR?.(eventData);
+                return replayMessage(H5Events.openOCR, REPLAY_WAIT_APP_CALLBACK)
+            }
+            if (eventType === H5Events.getLocation) {
+                const requestResult = await requestPermission(PermissionCode.Location);
+                if (requestResult != RESULTS.GRANTED) return
+                const data = await getLocation()
+                return replayMessage(eventType, data)
+            }
+
+            if (eventType === H5Events.webLoginSuccess) {
+                onWebLoginSuccess?.(eventData)
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+
+            if (eventType === H5Events.reload) {
+                webviewRef.current?.reload()
+                return replayMessage(eventType, NOT_NEED_REPLAY)
+            }
+            console.error(`[xm-rn-webview]: 未知事件${eventType}-->${eventData})`);
+        } catch (error: any) {
+            console.error(`[xm-rn-webview]: ${JSON.stringify(error)}`);
+        }
+
+    }
+    return (
+
+        <WebView
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            allowsInlineMediaPlayback={true}
+            mediaCapturePermissionGrantType={"grant"}
+            allowFileAccess
+            allowFileAccessFromFileURLs
+            source={{ uri: url }}
+            ref={webviewRef}
+            onMessage={receiveMessageFromH5}
+            style={{ flex: 1 }}
+        />
+    );
+}
